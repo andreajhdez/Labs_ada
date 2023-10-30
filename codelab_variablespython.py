b6043582d8af10626b8ae23dbf1d3327a191362e
andreajhdez <85519006+andreajhdez@users.noreply.github.com>
2023-10-29 22:49:33 -0500

    Create codelab_variablespython.py

---
+++ codelab_variablespython.py
@@ -0,0 +1,18 @@
+#Se definen variables de cada tipo de primitivo
+num_entero = 31
+num_real = 12.31
+tipo_cadena = "Hola me llamo Andrea Juliana"
+tipo_bool = False
+
+#Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
+concat_variables = str(num_entero) + " " + str(num_real) + " " + tipo_cadena + " " + str(tipo_bool) 
+
+# LIMITE ENTEROS Y FLOTANTES:
+investiga = "yadadyadya"
+
+#Aplica la fórmula de la suma de los primeros n números pares (investigar), tomando como n la variable de tipo entero y almacenar el resultado en una variable
+
+#Se imprimen los resultados de las tareas anteriores
+print(num_entero, num_real, tipo_cadena, tipo_bool, sep = '\n')
+print(concat_variables)
+print(investiga)
